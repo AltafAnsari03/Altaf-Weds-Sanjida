1037cf6bf155220a38a1b48e5594eb251817dc1b
AltafAnsari03 <mr.developer1104@gmail.com>
2026-09-22 12:43:13 +0530

    Update RoyalDoorIntro.tsx

--- src/components/sections/RoyalDoorIntro.tsx
+++ src/components/sections/RoyalDoorIntro.tsx
@@ -57,7 +57,7 @@ export default function RoyalDoorIntro({
           >
             <div className="flex justify-center mb-4">
               <img
-                src="/Altaf-Weds-Sanjida/logo.png"
+                src="/Altaf-Weds-Sanjida/public/logo.png"
                 alt="Wedding Logo"
                 className="w-32 h-32 object-contain"
               />
